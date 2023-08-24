12df82ec05379a7c0720d80405c59177703b5212
Vladimir Samorodov <vovchikan@gmail.com>
2023-08-24 05:58:07 +0000

    Remove kitty

--- scripts/install.sh
+++ scripts/install.sh
@@ -127,28 +127,6 @@ EOF
 
   echo
   echo "+---------------------------------+"
-  echo "|        Installing kitty         |"
-  echo "+---------------------------------+"
-  echo
-  curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-
-  # Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in
-  # your system-wide PATH)
-  mkdir -vp ~/.local/bin
-  ln -sf ~/.local/kitty.app/bin/kitty ~/.local/bin/
-
-  # Place the kitty.desktop file somewhere it can be found by the OS
-  mkdir -vp ~/.local/share/applications
-  cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
-  # If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
-  cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
-  # Update the paths to the kitty and its icon in the kitty.desktop file(s)
-  sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
-  sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
-
-
-  echo
-  echo "+---------------------------------+"
   echo "|        Installing rclone        |"
   echo "+---------------------------------+"
   echo
@@ -157,11 +135,10 @@ EOF
 
   echo
   echo "+---------------------------------+"
-  echo "|        Installing gitk,         |"
-  echo "|         qBittorrent             |"
+  echo "|        Installing gitk          |"
   echo "+---------------------------------+"
   echo
-  sudo apt install gitk qbittorrent
+  sudo apt install gitk
 
 }
 
