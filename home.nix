e5f98c353ea29e8cad64a27df3ea2eaa8fecc1ab
Vladimir Samorodov <vovchikan@gmail.com>
2023-12-25 14:17:45 +0500

    openconnect (unchecked)

--- home.nix
+++ home.nix
@@ -24,6 +24,11 @@
     pkgs.screen
     pkgs.neofetch
 
+    # openconnect
+    #pkgs.openconnect
+    #pkgs.networkmanager-openconnect
+    #pkgs.gnome.networkmanager-openconnect
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
