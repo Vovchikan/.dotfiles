439815029cc50ba17330e69d3c02f556b907828e
Vladimir Samorodov <vovchikan@gmail.com>
2023-12-26 21:58:26 +0500

    Remove openconnect from home manager

--- home.nix
+++ home.nix
@@ -24,11 +24,6 @@
     pkgs.screen
     pkgs.neofetch
 
-    # openconnect
-    #pkgs.openconnect
-    #pkgs.networkmanager-openconnect
-    #pkgs.gnome.networkmanager-openconnect
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
