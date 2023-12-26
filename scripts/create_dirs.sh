c4e816645fe5da3580a30dd369bb01b7e16c9086
Vladimir Samorodov <vovchikan@gmail.com>
2023-12-26 22:31:15 +0500

    Create directories

---
+++ scripts/create_dirs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+mkdir -pv ~/dvp/elixir
+mkdir -pv ~/dvp/sql
+mkdir -pv ~/dvp/work
+
+mkdir -pv ~/Downloads/Firefox
+mkdir -pv ~/Downloads/Torrent
+mkdir -pv ~/Downloads/Telegram
+mkdir -pv ~/Downloads/Vivaldi
+mkdir -pv ~/Downloads/Chromium
+
+mkdir -pv ~/Documents/keepassxc
